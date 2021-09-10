75786f7efdd85b9e56e34ffc8c4bcf01abf27559
yhupaul <yhupaul@hotmail.com>
2021-09-09 18:43:38 -0700

    fix minor function

--- src/components/Application.js
+++ src/components/Application.js
@@ -57,6 +57,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {schedule}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
